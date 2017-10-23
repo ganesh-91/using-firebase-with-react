16601f0e1af967f7f12b3406a57009718a7b98e5
ganesh-91 <ganesh.khaire91@gmail.com>
2017-10-23 12:14:11 +0530

    added firewbase db

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: "xzc",
+      userList: []
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref();
+    const userRef = rootRef.child('user');
+    const userListRef = rootRef.child('users');
+
+    userRef.on('value', snap => {
+      this.setState({
+        userName: snap.val()
+      })
+    });
+
+    userListRef.on('value', snap => {
+      this.setState({
+        userList: snap.val()
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +40,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{this.state.userName}</p>
+        {this.state.userList.map((user) => {
+          return (<p>{user.age}</p>)
+        })}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+let config = {
+    apiKey: "AIzaSyDFqxSjH5pJG4xwwM1BJ7Ng40lgVm495B0",
+    authDomain: "fir-poc-12602.firebaseapp.com",
+    databaseURL: "https://fir-poc-12602.firebaseio.com",
+    projectId: "fir-poc-12602",
+    storageBucket: "fir-poc-12602.appspot.com",
+    messagingSenderId: "641558267156"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
